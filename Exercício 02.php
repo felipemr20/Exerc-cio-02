ea01545043e364798c6d8011fa8eab8181d3dff0
Felipe Martins <61167608+felipemr20@users.noreply.github.com>
2022-03-17 08:40:24 -0300

    Add files via upload

---
+++ Exercício 02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Meu primeiro Código</title>
+    </head>
+    <body>
+        <?php
+         echo '<h2>Exercício 02</h2>';
+		 //comentario
+		 $variavel = "esta é uma string";
+		 echo "<br>";
+		 echo $variavel;
+		 $variavel = 5 + 3;
+		 echo "<br>";
+		 echo $variavel;
+        ?>
+		<br>
+    </body>
+</html>
